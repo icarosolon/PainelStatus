44550b2d409f7b93aafbeb886b2ffad9d47d1027
unknown <icaro.solon.is@gmail.com>
2020-02-22 17:52:34 -0300

    reajustes no layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,131 +29,129 @@
                     <div class="col-lg table-danger">
                     <table class="table table-hover" >
                             <thead > 
-                              <tr class="bg-danger" align="center" style="color:white;"> 
-                                <th colspan="4" ><h3>Urgência / Emergência</h3></th>    
-                            <tr align="center"class="bg-danger " style="color:white;"><!-- https://getbootstrap.com/docs/4.4/content/tables/ -->
-                              <th class="align-middle">Ac NB</th>
-                              <th class="align-middle">Paciente</th>
-                              <th class="align-middle">Exame</th>
-                              <th class="align-middle">Envio PACS</th>
+                            <tr class="bg-danger" align="center" style="color:#5e1914;">
+                                <th colspan="1" >
+                                    <h3><strong>Urgência / Emergência</strong> 
+                                    </h3>
+                                </th>
                             </tr>
-                          </thead>
-                          <tbody>
-                            <tr align="center">
-                              <th class="align-middle">19246</th>
-                              <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                              <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                              <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
+                            </thead>
+                            <tbody>
+                            <tr class="table-danger "><!-- LINHA COM DADOS DO PACIENTE -->
+                                <td>
+                                    <p>
+                                        <strong>Ac. Number: <b style="font-size:26px;"> 656465</b></strong><br>
+                                        <strong>Paciente: </strong>JOSE CARLOS MAGALHAES FILHO<br>
+                                        <strong>Exame:</strong> US ABSOME TOTAL (INCLUI ABDOME INFERIOR) 
+                                    </p> 
+                                </td>
                             </tr>
-                            <tr align="center">
-                                <th class="align-middle">19246</th>
-                                <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
+                            <tr class="table-danger "><!-- LINHA COM DADOS DO PACIENTE -->
+                                <td>
+                                    <p>
+                                        <strong>Ac. Number: <b style="font-size:26px;"> 656465</b></strong><br>
+                                        <strong>Paciente: </strong>JOSE CARLOS MAGALHAES FILHO<br>
+                                        <strong>Exame:</strong> US ABSOME TOTAL (INCLUI ABDOME INFERIOR) 
+                                    </p> 
+                                </td>
                             </tr>
-                            <tr align="center">
-                                <th class="align-middle">19246</th>
-                                <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
+                            <tr class="table-danger "><!-- LINHA COM DADOS DO PACIENTE -->
+                                <td>
+                                    <p>
+                                        <strong>Ac. Number: <b style="font-size:26px;"> 656465</b></strong><br>
+                                        <strong>Paciente: </strong>JOSE CARLOS MAGALHAES FILHO<br>
+                                        <strong>Exame:</strong> US ABSOME TOTAL (INCLUI ABDOME INFERIOR) 
+                                    </p> 
+                                </td>
                             </tr>
                           </tbody>
                         </table>
                     </div>
                     <!--Segunda coluna INTERNAMENTO-->
-                    <div class="col-sm table-warning">
-                        <table class="table table-hover" >
-                                <thead > 
-                                    <tr class="bg-warning" align="center" style="color:white;"> 
-                                    <th colspan="4" ><h3>Urgência / Emergência</h3></th>    
-                                <tr align="center"class="bg-warning " style="color:white;"><!-- https://getbootstrap.com/docs/4.4/content/tables/ -->
-                                    <th class="align-middle">Ac NB</th>
-                                    <th class="align-middle">Paciente</th>
-                                    <th class="align-middle">Exame</th>
-                                    <th class="align-middle">Envio PACS</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr align="center">
-                                    <th class="align-middle">19246</th>
-                                    <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                    <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                    <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
-                                </tr>
-                                <tr align="center">
-                                    <th class="align-middle">19246</th>
-                                    <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                    <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                    <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
-                                </tr>
-                                <tr align="center">
-                                    <th class="align-middle">19246</th>
-                                    <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                    <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                    <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
-                                </tr>
-                                <tr align="center">
-                                        <th class="align-middle">19246</th>
-                                        <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                        <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                        <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
+                    <div class="col-lg table-warning">
+                            <table class="table table-hover" >
+                                    <thead > 
+                                    <tr class="bg-warning" align="center" style="color:#cc7722;">
+                                        <th colspan="1" >
+                                            <h3><strong>Internamento</strong>
+                                            </h3>
+                                        </th>
                                     </tr>
-                                    <tr align="center">
-                                            <th class="align-middle">19246</th>
-                                            <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                            <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                            <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
-                                        </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                                    </thead>
+                                    <tbody>
+                                    <tr class="table-warning "><!-- LINHA COM DADOS DO PACIENTE -->
+                                        <td>
+                                            <p>
+                                                <strong>Ac. Number: <b style="font-size:26px;"> 656465</b></strong><br>
+                                                <strong>Paciente: </strong>JOSE CARLOS MAGALHAES FILHO<br>
+                                                <strong>Exame:</strong> US ABSOME TOTAL (INCLUI ABDOME INFERIOR) 
+                                            </p> 
+                                        </td>
+                                    </tr>
+                                    <tr class="table-warning "><!-- LINHA COM DADOS DO PACIENTE -->
+                                        <td>
+                                            <p>
+                                                <strong>Ac. Number: <b style="font-size:26px;"> 656465</b></strong><br>
+                                                <strong>Paciente: </strong>JOSE CARLOS MAGALHAES FILHO<br>
+                                                <strong>Exame:</strong> US ABSOME TOTAL (INCLUI ABDOME INFERIOR) 
+                                            </p> 
+                                        </td>
+                                    </tr>
+                                    <tr class="table-warning "><!-- LINHA COM DADOS DO PACIENTE -->
+                                        <td>
+                                            <p>
+                                                <strong>Ac. Number: <b style="font-size:26px;"> 656465</b></strong><br>
+                                                <strong>Paciente: </strong>JOSE CARLOS MAGALHAES FILHO<br>
+                                                <strong>Exame:</strong> US ABSOME TOTAL (INCLUI ABDOME INFERIOR) 
+                                            </p> 
+                                        </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                            </div>
+                    
                         <!--Terceira coluna EXTERNO-->
-                        <div class="col-sm table-success">
-                            <table class="table table-hover " >
-                                <thead > 
-                                    <tr class="bg-success" align="center" style="color:white;"> 
-                                    <th colspan="4" ><h3>Urgência / Emergência</h3></th>    
-                                <tr align="center"class="bg-success " style="color:white;"><!-- https://getbootstrap.com/docs/4.4/content/tables/ -->
-                                    <th class="align-middle">Ac NB</th>
-                                    <th class="align-middle">Paciente</th>
-                                    <th class="align-middle">Exame</th>
-                                    <th class="align-middle">Envio PACS</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr align="center">
-                                    <th class="align-middle">19246</th>
-                                    <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                    <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                    <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
-                                </tr>
-                                <tr align="center">
-                                    <th class="align-middle">19246</th>
-                                    <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                    <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                    <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
-                                </tr>
-                                <tr align="center">
-                                    <th class="align-middle">19246</th>
-                                    <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                    <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                    <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
-                                </tr>
-                                <tr align="center">
-                                        <th class="align-middle">19246</th>
-                                        <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                        <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                        <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
-                                    </tr>
-                                    <tr align="center">
-                                            <th class="align-middle">19246</th>
-                                            <td class="align-middle">DANIEL JOSE DA SILVA XAVIER</td>
-                                            <td class="align-middle">US ABSOME TOTAL (INCLUI ABDOME INFERIOR)</td>
-                                            <td class="align-middle"><input type="checkbox" name="realizado" id="realizado"></td>
+                        <div class="col-lg table-success">
+                                <table class="table table-hover" >
+                                        <thead > 
+                                        <tr class="bg-success" align="center" style="color:#0b6623;">
+                                            <th colspan="1" >
+                                                <h3><strong>Externo</strong>
+                                                </h3>
+                                            </th>
                                         </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                                        </thead>
+                                        <tbody>
+                                        <tr class="table-success "><!-- LINHA COM DADOS DO PACIENTE -->
+                                            <td>
+                                                <p>
+                                                    <strong>Ac. Number: <b style="font-size:26px;"> 656465</b></strong><br>
+                                                    <strong>Paciente: </strong>JOSE CARLOS MAGALHAES FILHO<br>
+                                                    <strong>Exame:</strong> US ABSOME TOTAL (INCLUI ABDOME INFERIOR) 
+                                                </p> 
+                                            </td>
+                                        </tr>
+                                        <tr class="table-success "><!-- LINHA COM DADOS DO PACIENTE -->
+                                            <td>
+                                                <p>
+                                                    <strong>Ac. Number: <b style="font-size:26px;"> 656465</b></strong><br>
+                                                    <strong>Paciente: </strong>JOSE CARLOS MAGALHAES FILHO<br>
+                                                    <strong>Exame:</strong> US ABSOME TOTAL (INCLUI ABDOME INFERIOR) 
+                                                </p> 
+                                            </td>
+                                        </tr>
+                                        <tr class="table-success"><!-- LINHA COM DADOS DO PACIENTE -->
+                                            <td>
+                                                <p>
+                                                    <strong>Ac. Number: <b style="font-size:26px;"> 656465</b></strong><br>
+                                                    <strong>Paciente: </strong>JOSE CARLOS MAGALHAES FILHO<br>
+                                                    <strong>Exame:</strong> US ABSOME TOTAL (INCLUI ABDOME INFERIOR) 
+                                                </p> 
+                                            </td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                </div>
             </div>
         </div>
         
